b9630b720329a060012b4f5fe6653519672dcfa8
JuanFelipeCB01 <jfelipecbermudez@gmail.com>
2023-08-30 19:03:53 +0200

    recipes controller/router

--- Back/index.js
+++ Back/index.js
@@ -1,28 +1,33 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { connect } = require('./src/utils/database');
 const cors = require('cors');
 const cloudinary = require('cloudinary').v2;
-
-const { connect } = require('./src/utils/database');
-const routerUser = require('./src/api/routes/user.routes');
+const recipeRouter = require('./src/api/routes/recipes.route');
 
 dotenv.config();
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
-  secure: true,
-});
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_NAME,
+//   api_key: process.env.CLOUDINARY_KEY,
+//   api_secret: process.env.CLOUDINARY_SECRET,
+//   secure: true,
+// });
 
 const PORT = process.env.PORT || 5001;
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin:"*",
+    credentials:true
+  }
+));
 app.use(express.json());
+
+app.use('/recipes', recipeRouter);
 
 connect();
 
-// bloque de rutas
-//app.use('/user', routerUser);
+
 
 app.listen(PORT, () => {
   console.log(`Listening http://localhost:${PORT}`);
