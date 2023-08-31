1c70afd008c9621c4f4d4dc36bb9d3bbe3f42c7c
pablosierradev <pablosierra.developer@gmail.com>
2023-09-01 00:07:24 +0200

    login-register

--- Front/the-recipe/src/pages/LoginPage/LoginPage.jsx
+++ Front/the-recipe/src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import {useNavigate} from "react-router-dom"
+import { axiosInstance } from "../../utils/axios.js";
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-function LoginPage(){
-    return (
-        <h1>LoginPage</h1>
-    )
-}
+    try {
+      const response = await axiosInstance.post("/user/login", {
+        email,
+        password,
+      });
+      // console.log(response.data)
+      const token = response.data.token;
+      localStorage.setItem("token", token);
+      navigate("/");
 
-export default LoginPage
+    } catch (err) {
+      setError("Invalid credentials. Please try again.");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default Login;
