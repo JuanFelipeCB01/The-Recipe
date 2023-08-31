b0ba1317efe02a5665acfbf95287ef27d797f01f
pablosierradev <pablosierra.developer@gmail.com>
2023-08-31 16:36:58 +0200

    front-setup

---
+++ Front/the-recipe/src/pages/IngredientsDetailPage/IngredientsDetailPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function IngredientsDetailPage(){
+    return (
+        <h1>IngredientsDetailPage</h1>
+    )
+}
+
+export default IngredientsDetailPage
---
+++ Front/the-recipe/src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function RegisterPage(){
+    return (
+        <h1>RegisterPage</h1>
+    )
+}
+
+export default RegisterPage
